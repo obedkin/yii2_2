b859d5f1941ecb8ad23e189144e68c4b7c239433
obedkin <obedkin@gmail.com>
2016-12-04 01:03:49 +0300

    Урок 5. Кэширование списка категорий

--- components/MenuWidget.php
+++ components/MenuWidget.php
@@ -9,6 +9,7 @@
 namespace app\components;
 use yii\base\Widget;
 use app\models\Category;
+use Yii;
 
 class MenuWidget extends Widget{
 
@@ -26,9 +27,15 @@ class MenuWidget extends Widget{
     }
 
     public function run(){
+        // get cache
+        $menu = Yii::$app->cache->get('menu');
+        if($menu) return $menu;
+
         $this->data = Category::find()->indexBy('id')->asArray()->all();
         $this->tree = $this->getTree();
         $this->menuHtml = $this->getMenuHtml($this->tree);
+        // set cache
+        Yii::$app->cache->set('menu', $this->menuHtml, 60);
         return $this->menuHtml;
     }
 
