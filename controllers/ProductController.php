bd5b860f3f45b084c62ff2aea57a367a5c056c3c
obedkin <obedkin@gmail.com>
2016-12-04 20:23:50 +0300

    Урок 11. Карточка товара. Часть 2

--- controllers/ProductController.php
+++ controllers/ProductController.php
@@ -17,7 +17,9 @@ class ProductController extends AppController{
         $id = Yii::$app->request->get('id');
         $product = Product::findOne($id);
 //        $product = Product::find()->with('category')->where(['id' => $id])->limit(1)->one();
-        return $this->render('view', compact('product'));
+        $hits = Product::find()->where(['hit' => '1'])->limit(6)->all();
+        $this->setMeta('E-SHOPPER | ' . $product->name, $product->keywords, $product->description);
+        return $this->render('view', compact('product', 'hits'));
     }
 
 } 
