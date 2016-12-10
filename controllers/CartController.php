b6bd24d54a8a4126d3bdf4896078fa421d30b0ad
obedkin <obedkin@gmail.com>
2016-12-10 23:44:30 +0300

    Урок 19. Оформление заказа. Часть 1

--- controllers/CartController.php
+++ controllers/CartController.php
@@ -9,6 +9,8 @@
 namespace app\controllers;
 use app\models\Product;
 use app\models\Cart;
+use app\models\Order;
+use app\models\OrderItems;
 use Yii;
 
 /*Array
@@ -79,7 +81,14 @@ class CartController extends AppController{
     }
 
     public function actionView(){
-        return $this->render('view');
+        $session =Yii::$app->session;
+        $session->open();
+        $this->setMeta('Корзина');
+        $order = new Order();
+        if( $order->load(Yii::$app->request->post()) ){
+            debug(Yii::$app->request->post());
+        }
+        return $this->render('view', compact('session', 'order'));
     }
 
 } 
