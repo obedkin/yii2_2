d67ecddf51a900fede86587d972ea6448d56b9d9
obedkin <obedkin@gmail.com>
2016-12-11 17:15:33 +0300

    Урок 22. Авторизация. Часть 2

--- config/web.php
+++ config/web.php
@@ -26,6 +26,7 @@ $config = [
         'user' => [
             'identityClass' => 'app\models\User',
             'enableAutoLogin' => true,
+//            'loginUrl' => 'cart/view'
         ],
         'errorHandler' => [
             'errorAction' => 'site/error',
