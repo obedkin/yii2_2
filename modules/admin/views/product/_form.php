140b979651605c89fc0876514ad79aa9bb32bb6e
obedkin <obedkin@gmail.com>
2016-12-25 16:45:44 +0300

    Урок 30. Загрузка картинок. Часть 1

--- modules/admin/views/product/_form.php
+++ modules/admin/views/product/_form.php
@@ -13,7 +13,7 @@ mihaildev\elfinder\Assets::noConflict($this);
 
 <div class="product-form">
 
-    <?php $form = ActiveForm::begin(); ?>
+    <?php $form = ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data']]); ?>
 
     <div class="form-group field-product-category_id has-success">
         <label class="control-label" for="product-category_id">Родительская категория</label>
@@ -25,12 +25,6 @@ mihaildev\elfinder\Assets::noConflict($this);
     <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
 
     <?php
-    /*echo $form->field($model, 'content')->widget(CKEditor::className(), [
-        'editorOptions' => [
-            'preset' => 'full', //разработанны стандартные настройки basic, standard, full данную возможность не обязательно использовать
-            'inline' => false, //по умолчанию false
-        ],
-    ]);*/
     ?>
     <?php
     echo $form->field($model, 'content')->widget(CKEditor::className(), [
@@ -44,7 +38,7 @@ mihaildev\elfinder\Assets::noConflict($this);
 
     <?= $form->field($model, 'description')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'img')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'image')->fileInput() ?>
 
     <?= $form->field($model, 'hit')->checkbox([ '0', '1', ]) ?>
 
